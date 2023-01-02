4f8efffcfdb07b12c829086ec613414553fe1130
Roman Kavyrshin <rom.kawirshin@yandex.ru>
2023-01-02 10:37:50 +0300

    Изменен способ генерации соли. Закомментированы логи

--- app/src/main/java/ru/kavyrshin/aesexample/UserKeyActivity.kt
+++ app/src/main/java/ru/kavyrshin/aesexample/UserKeyActivity.kt
@@ -2,12 +2,10 @@ package ru.kavyrshin.aesexample
 
 import android.os.Bundle
 import android.util.Base64
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.lambdapioneer.argon2kt.Argon2Kt
-import com.lambdapioneer.argon2kt.Argon2KtUtils
 import com.lambdapioneer.argon2kt.Argon2Mode
 import com.lambdapioneer.argon2kt.Argon2Version
 import io.reactivex.Single
@@ -17,7 +15,6 @@ import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_user_key.*
 import java.security.GeneralSecurityException
 import java.security.SecureRandom
-import java.util.*
 import javax.crypto.Cipher
 import javax.crypto.SecretKey
 import javax.crypto.spec.IvParameterSpec
@@ -38,15 +35,15 @@ class UserKeyActivity : AppCompatActivity() {
         buttonEncrypt.setOnClickListener {
             val random = SecureRandom()
             val openTextBeforePad = editTextOpentext.text.toString().toByteArray(Charsets.UTF_8)
-            Log.d("myLogs", "utf-8 bytes  ${openTextBeforePad.contentToString()}")
-            Log.d("myLogs", "utf-8 length ${openTextBeforePad.size}")
+//            Log.d("myLogs", "utf-8 bytes  ${openTextBeforePad.contentToString()}")
+//            Log.d("myLogs", "utf-8 length ${openTextBeforePad.size}")
 
             val paddingNeededSize = 16 - (openTextBeforePad.size % 16)
             val noPadding = ByteArray(paddingNeededSize).also {
                 random.nextBytes(it)
             }
             val openText = openTextBeforePad + noPadding
-            Log.d("myLogs", "openTextSize ${openText.size}; openText ${openText.contentToString()}")
+//            Log.d("myLogs", "openTextSize ${openText.size}; openText ${openText.contentToString()}")
 
             compositeDisposable.add(generateAesKey(editTextKey.text.toString(), SALT)
                 .subscribeOn(Schedulers.computation())
@@ -59,7 +56,7 @@ class UserKeyActivity : AppCompatActivity() {
                         secretKey = secretKey
                     )
 
-                    Log.d("myLogs", "iv + cipherText ${Arrays.toString(cipherText)}")
+//                    Log.d("myLogs", "iv + cipherText ${Arrays.toString(cipherText)}")
 
                     val cipherString = String(Base64.encode(cipherText, Base64.DEFAULT), Charsets.UTF_8)
                     editTextCiphertext.setText(cipherString)
@@ -82,7 +79,7 @@ class UserKeyActivity : AppCompatActivity() {
                     cipherText to secretKey
                 }
                 .map { (cipherText, secretKey) ->
-                    Log.d("myLogs", "cipherText.length ${cipherText.size}")
+//                    Log.d("myLogs", "cipherText.length ${cipherText.size}")
                     decryptMessage(cipherText, secretKey)
                 }
                 .subscribe({ plainText ->
@@ -109,11 +106,11 @@ class UserKeyActivity : AppCompatActivity() {
 
         cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivSpec)
 
-        Log.d("myLogs", "encryptMessage: IV from cipher ${Arrays.toString(cipher.iv)}")
+//        Log.d("myLogs", "encryptMessage: IV from cipher ${Arrays.toString(cipher.iv)}")
 
         val ciphertext: ByteArray = cipher.doFinal(openText)
 
-        Log.d("myLogs", "cipherText.length ${ciphertext.size}")
+//        Log.d("myLogs", "cipherText.length ${ciphertext.size}")
 
         return cipher.iv + ciphertext
     }
@@ -128,15 +125,16 @@ class UserKeyActivity : AppCompatActivity() {
 
         cipher.init(Cipher.DECRYPT_MODE, secretKey, ivSpec)
 
-        Log.d("myLogs", "decryptMessage: blockSize ${cipher.blockSize}")
-        Log.d("myLogs", "decryptMessage: IV ${Arrays.toString(cipher.iv)}")
+//        Log.d("myLogs", "decryptMessage: blockSize ${cipher.blockSize}")
+//        Log.d("myLogs", "decryptMessage: IV ${Arrays.toString(cipher.iv)}")
 
         return cipher.doFinal(cipherText, cipher.blockSize, cipherText.size - cipher.blockSize)
     }
 
     private fun generateAesKey(userKey: String, salt: String): Single<SecretKey> { //TODO: Почему то отрабытывает быстрее чем в sample с теми же параметрами
-        val saltByteArray = Argon2KtUtils.decodeAsHex(salt)
-        Log.d("myLogs", "saltByteArray: ${saltByteArray.contentToString()}")
+//        val saltByteArray = Argon2KtUtils.decodeAsHex(salt)
+        val saltByteArray = salt.toByteArray(Charsets.US_ASCII)
+//        Log.d("myLogs", "saltByteArray: ${saltByteArray.contentToString()}")
         return Single.fromCallable {
             val result = argon2Kt.hash(
                 mode = Argon2Mode.ARGON2_ID,
@@ -150,7 +148,7 @@ class UserKeyActivity : AppCompatActivity() {
             )
 
             val rawHash = result.rawHashAsByteArray()
-            Log.d("myLogs", "rawHash length: ${rawHash.size}; content: ${rawHash.contentToString()}")
+//            Log.d("myLogs", "rawHash length: ${rawHash.size}; content: ${rawHash.contentToString()}")
 
             SecretKeySpec(rawHash, "AES")
         }
